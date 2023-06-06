ff093c797a95b8392d36ab454e4c67f92e27a7db
Oskar Przybylski <oskar.przybylski23@gmail.com>
2023-06-06 23:07:27 +0100

    added script to change navLinks colours

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
-  // JavaScript code in your script.js file
-  document.addEventListener('DOMContentLoaded', function() {
-    // Get all the navigation links
-    var navLinks = document.querySelectorAll('.link');
+// Script to change navigation bar colours based on page section
+document.addEventListener('DOMContentLoaded', function() {
+  // Get all the navigation links
+  var navLinks = document.querySelectorAll('.link');
 
-    // Calculate the position of each section on the page
-    var sections = document.querySelectorAll('section');
-    var sectionPositions = Array.from(sections).map(function(section) {
+  // Calculate the position of each section on the page
+  var sections = document.querySelectorAll('section');
+  var footer = document.querySelector('footer');
+  var sectionPositions = [];
+
+  function calculateSectionPositions() {
+    sectionPositions = Array.from(sections).map(function(section) {
       return {
         id: section.id,
         top: section.offsetTop,
         bottom: section.offsetTop + section.offsetHeight
       };
     });
+  }
 
-    // Add scroll event listener to track the position
-    window.addEventListener('scroll', function() {
-      var currentPosition = window.pageYOffset;
+  calculateSectionPositions(); // Initial calculation of section positions
+  console.log(sectionPositions);
 
-      // Reset the color of all navigation links
-      navLinks.forEach(function(link) {
-        link.style.color = '';
-      });
+  // Recalculate section positions on window resize
+  window.addEventListener('resize', function() {
+    calculateSectionPositions();
+  });
 
+  // Add scroll event listener to track the position
+  window.addEventListener('scroll', function() {
+    var currentPosition = window.pageYOffset;
+
+    // Reset the color of all navigation links
+    navLinks.forEach(function(link) {
+      link.style.color = '';
+    });
+
+    // Check if scroll position is within the footer section
+    var isOverFooter = currentPosition >= footer.offsetTop;
+
+    if (!isOverFooter) {
       // Find the current section based on the scroll position
       var currentSection = sectionPositions.find(function(section) {
         return currentPosition >= section.top && currentPosition < section.bottom;
       });
-
-      var secondSection = document.querySelector('#about');
-      var thirdSection = document.querySelector('#why');
-      var secondSectionTop = secondSection.offsetTop;
-      var secondSectionBottom = secondSection.offsetTop + secondSection.offsetHeight;
-      var thirdSectionTop = thirdSection.offsetTop;
-      var thirdSectionBottom = thirdSection.offsetTop + thirdSection.offsetHeight;
-
+      console.log(currentSection);
       // If a current section is found, change the color of its corresponding navigation link
       if (currentSection) {
         var currentLink = document.querySelector('a[href="#' + currentSection.id + '"]');
@@ -42,11 +52,27 @@
         }
       }
 
-      // Change the color of all links if the scroll position is within the second section
-      if (currentPosition >= secondSectionTop && currentPosition < thirdSectionBottom) {
+      // Change the color of all links if the scroll position is within the second and fourth sections
+      if (currentPosition >= sectionPositions[1].top && currentPosition < sectionPositions[3].bottom) {
         navLinks.forEach(function(link) {
-          link.style.color = 'var(--font2)'; // Set the desired color
+          if (link.style.color !== 'var(--secondary)') {
+            link.style.color = 'var(--font2)'; // Set the desired color
+          }
         });
       }
-    });
+
+      // Show navigation links if they were hidden
+      navLinks.forEach(function(link) {
+        link.style.display = '';
+      });
+    } else {
+      // Hide navigation links when scrolling over the footer
+      navLinks.forEach(function(link) {
+        link.style.display = 'none';
+      });
+    }
   });
+});
+
+
+// 
